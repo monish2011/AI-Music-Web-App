571d86ab3345529ebba68c8ea25717019dbc6468
monish2011 <142167411+monish2011@users.noreply.github.com>
2024-06-22 11:44:57 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+music1="";
+music2="";
 function setup(){
     canvas = createCanvas(500,400);
     canvas.center();
+ 
     video = createCapture(VIDEO);
     video.hide();
 }
@@ -8,3 +11,7 @@ function draw(){
     image(video,0,0,500,400);
 }
 
+function preload(){
+    music1= loadSound("harry_potter.mp3");
+    music2 = loadSound("peter_pan.mp3");
+}
