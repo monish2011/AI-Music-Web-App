cb3562d3b11e28722a2bbe629f689fcfec29b799
monish2011 <142167411+monish2011@users.noreply.github.com>
2024-06-22 11:49:04 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 music1="";
 music2="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function setup(){
     canvas = createCanvas(500,400);
     canvas.center();
  
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 function draw(){
     image(video,0,0,500,400);
@@ -15,3 +21,16 @@ function preload(){
     music1= loadSound("harry_potter.mp3");
     music2 = loadSound("peter_pan.mp3");
 }
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+    
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+
+}}
+
