d3e27ce7f148604929fe7bbfccc3e9ef3a39a674
monish2011 <142167411+monish2011@users.noreply.github.com>
2024-06-24 09:24:15 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist =0;
+song_status = "";
 function setup(){
     canvas = createCanvas(500,400);
     canvas.center();
@@ -15,11 +17,24 @@ function setup(){
 }
 function draw(){
     image(video,0,0,500,400);
+    fill("#FF0000");
+    stroke("#FF0000");
+    song_status=music1.isPlaying();
+    if(scoreLeftWrist > 0.2){
+        circle(lefWristX,leftWristY, 20);
+        music2.stop();
+        
+    }
+    if(song_status == false){
+        music1.play();
+        document.getElementById("song_name").innerHTML = "Harry Potter Theme Song";
+    }
 }
 
 function preload(){
     music1= loadSound("harry_potter.mp3");
     music2 = loadSound("peter_pan.mp3");
+    
 }
 function gotPoses(results){
     if(results.length>0){
@@ -27,6 +42,7 @@ function gotPoses(results){
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+        scoreLeftWrist= results[0].pose.keypoints[9].score;
     
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
@@ -34,3 +50,6 @@ function gotPoses(results){
 
 }}
 
+function modelLoaded(){
+    console.log("PoseNet has been intialized!");
+}
